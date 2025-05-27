e55f0b53ee83a49b8b0e624d06ec2cfa3d5c643d
finessestyle <finessestyle1@gmail.com>
2025-05-28 00:12:38 +0900

    20250528

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,14 +3,56 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = () => {
   return(
     <View style={styles.container}>
-      <Text>Hello World</Text>
+
+      <View style={styles.inner}>
+        <View>
+          <Text>MemoApp</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年5月28日0:07</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+
+        <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年5月28日0:07</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+
+        <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年5月28日0:07</Text>
+        </View>
+        <View>
+          <Text>X</Text>
+        </View>
+
+      </View>
+
+      <View>
+        <Text>＋</Text>
+      </View>
+
     </View>
   )
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  inner: {
   }
 })
 
