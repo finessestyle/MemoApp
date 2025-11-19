88412c64b88ea538b50a82c7bd706afe2ea85286
finessestyle <finessestyle1@gmail.com>
2025-11-19 22:05:15 +0900

    Markup MemoList

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -3,7 +3,54 @@ import { View, Text, StyleSheet } from 'react-native'
 const Index = () => {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+
+      <View style={styles.header}>
+        <View>
+          <Text>Memo App</Text>
+          <Text>ログアウト</Text>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月19日21:58</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月19日21:58</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2025年11月19日21:58</Text>
+          </View>
+          <View>
+            <Text>x</Text>
+          </View>
+        </View>
+      </View>
+
+      <View>
+        <Text>＋</Text>
+      </View>
+
     </View>
   )
 }
