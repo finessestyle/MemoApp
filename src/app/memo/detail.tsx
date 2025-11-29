d76de60cb8a3e6aaeb327e46e7d3993bfd815738
finessestyle <finessestyle1@gmail.com>
2025-11-29 09:51:35 +0900

    edit

--- src/app/memo/detail.tsx
+++ src/app/memo/detail.tsx
@@ -7,17 +7,17 @@ import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 import { type Memo } from '../../../types/memo'
 
-const handlePress = () => {
-  router.push('/memo/edit')
+const handlePress = (id: string) => {
+  router.push( {pathname: '/memo/edit', params: { id } })
 }
 
 const Detail = (): ReactNode => {
-  const id = useLocalSearchParams()
+  const id = String(useLocalSearchParams().id)
   console.log(id)
   const [memo, setMemo] = useState<Memo | null>(null)
   useEffect(() => {
     if (auth.currentUser === null) return
-    const ref = doc(db, `/users/${auth.currentUser.uid}/memos`, String(id))
+    const ref = doc(db, `/users/${auth.currentUser.uid}/memos`, id)
     const unsubscrive =onSnapshot(ref, (memoDoc) => {
       const { bodyText, updatedAt } = memoDoc.data() as Memo
       setMemo({
@@ -46,7 +46,7 @@ const Detail = (): ReactNode => {
           {memo?.bodyText}
         </Text>
       </ScrollView>
-      <CircleButton style={{ top: 60 }} onPress={() => {handlePress()}}>
+      <CircleButton style={{ top: 60 }} onPress={() => {handlePress(id)}}>
         <Icon name='pencil' size={40} color='#ffffff' />
       </CircleButton>
     </View>
--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,26 +1,57 @@
-import { ReactNode } from 'react'
+import { ReactNode, useState, useEffect } from 'react'
 import {
-  ScrollView, TextInput, StyleSheet, KeyboardAvoidingView
+  ScrollView, TextInput, StyleSheet, KeyboardAvoidingView, Alert
 } from 'react-native'
-import { router } from 'expo-router'
+import { router, useLocalSearchParams } from 'expo-router'
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
+import { db, auth } from '../../config'
 import Icon from '../../components/Icon'
 import CircleButton from '../../components/CircleButton'
 
-const handlePress = () => {
-  router.push('/memo/detail')
+const handlePress = (id: string, bodyText: string) => {
+  if (auth.currentUser === null) return
+  const ref = doc(db, `/users/${auth.currentUser.uid}/memos`, id)
+  setDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+  .then (() => {
+    router.back()
+  })
+  .catch(() => {
+    Alert.alert('更新に失敗しました')
+  })
+
 }
 
 const Edit = (): ReactNode => {
+  const id = String(useLocalSearchParams().id)
+  const [bodyText, setBodyText] = useState('')
+  useEffect(() => {
+    if (auth.currentUser === null) return
+    const ref = doc(db, `/users/${auth.currentUser.uid}/memos`, id)
+    getDoc(ref)
+      .then((docRef) => {
+        console.log(docRef.data())
+        const RemoteBodyText = docRef.data()?.bodyText
+        setBodyText(RemoteBodyText)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
   return (
     <KeyboardAvoidingView behavior='height' style={styles.container}>
       <ScrollView style={styles.inputContainer}>
         <TextInput
           multiline
           style={styles.input}
-          value={'買い物\nリスト'}
+          value={bodyText}
+          autoFocus
+          onChangeText={(text) => {setBodyText(text)}}
         />
       </ScrollView>
-      <CircleButton onPress={() => {handlePress()}}>
+      <CircleButton onPress={() => {handlePress(id, bodyText)}}>
         <Icon name='check' size={40} color='#ffffff' />
       </CircleButton>
     </KeyboardAvoidingView>
@@ -32,15 +63,15 @@ const styles = StyleSheet.create({
     flex: 1
   },
   inputContainer: {
-    flex: 1,
-    paddingVertical: 32,
-    paddingHorizontal: 27
+    flex: 1
   },
   input: {
     flex: 1,
     textAlignVertical: 'top',
     fontSize: 16,
-    lineHeight: 24
+    lineHeight: 24,
+    paddingVertical: 32,
+    paddingHorizontal: 27
   }
 })
 
