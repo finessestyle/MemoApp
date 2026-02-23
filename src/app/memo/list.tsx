356269e6f929f30865e357b66c5eee6a7069d780
finessestyle <finessestyle1@gmail.com>
2026-02-23 21:19:28 +0900

    Markup memo list

--- src/app/memo/list.tsx
+++ src/app/memo/list.tsx
@@ -1,6 +1,6 @@
 import { View, Text, StyleSheet } from 'react-native'
 
-const Index = () => {
+const List = () => {
   return (
     <View style={styles.container}>
       <View>
@@ -9,37 +9,34 @@ const Index = () => {
           <Text>ログアウト</Text>
         </View>
       </View>
-      <View >
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2026年2月23日21:13</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2026年2月23日21:13</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-        <View>
-          <View>
-            <Text>買い物リスト</Text>
-            <Text>2026年2月23日21:13</Text>
-          </View>
-          <View>
-            <Text>×</Text>
-          </View>
-        </View>
-      </View>
       <View>
-        <Text>＋</Text>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2026年2月23日21:13</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2026年2月23日21:13</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
+        <View>
+          <View>
+            <Text>買い物リスト</Text>
+            <Text>2026年2月23日21:13</Text>
+          </View>
+          <View>
+            <Text>×</Text>
+          </View>
+        </View>
       </View>
     </View>
   )
@@ -47,10 +44,8 @@ const Index = () => {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
+    flex: 1
   }
 })
 
-export default Index
+export default List
