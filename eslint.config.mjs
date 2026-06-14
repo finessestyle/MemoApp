6fb6b7a5b5ce8418825305e1645415cd14dcd0c5
finessestyle <finessestyle1@gmail.com>
2026-06-14 17:18:34 +0900

    props

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,32 +1,43 @@
 import js from "@eslint/js"
 import globals from "globals"
 import tseslint from "typescript-eslint"
-import pluginReact from "eslint-plugin-react"
+import react from "eslint-plugin-react"
+import reactHooks from "eslint-plugin-react-hooks"
 
 export default [
   js.configs.recommended,
 
+  ...tseslint.configs.recommended,
+
   {
     files: ["**/*.{js,jsx,ts,tsx}"],
     languageOptions: {
-      globals: globals.es2021
-    }
-  },
+      globals: {
+        ...globals.es2021,
+        ...globals.node,
+        ...globals.browser
+      }
+    },
 
-  ...tseslint.configs.recommended,
+    plugins: {
+      react,
+      "react-hooks": reactHooks
+    },
 
-  pluginReact.configs.flat.recommended,
-
-  {
     settings: {
       react: {
         version: "detect"
       }
     },
+
     rules: {
-      "react/react-in-jsx-scope": "off",
+      ...reactHooks.configs.recommended.rules,
+      ...react.configs.recommended.rules,
+
       semi: ["error", "never"],
-      "comma-dangle": ["error", "never"]
+      "comma-dangle": ["error", "never"],
+
+      "react/react-in-jsx-scope": "off"
     }
   }
 ]
