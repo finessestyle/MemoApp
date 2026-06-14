454324b7683b493ab693fa28637530b7de53e062
finessestyle <finessestyle1@gmail.com>
2026-06-15 08:13:34 +0900

    memo/detail

---
+++ src/app/memo/detail.tsx
@@ -0,0 +1,57 @@
+import { View, Text, ScrollView, StyleSheet } from 'react-native'
+import Header from '../../components/Header'
+import CircleButton from '../../components/CircleButton'
+
+const Detail = () => {
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.memoHeader}>
+        <Text style={styles.memoTitle}>買い物リスト</Text>
+        <Text style={styles.memoDate}>2026年6月15日7:57</Text>
+      </View>
+      <ScrollView style={styles.memoBody}>
+        <Text style={styles.memoBodyText}>
+          買い物リスト
+          ・りんご
+          ・バナナ
+          ・いちご
+        </Text>
+      </ScrollView>
+      <CircleButton style={{ top: 170 }}>＋</CircleButton>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1
+  },
+  memoHeader: {
+    backgroundColor: '#467FD3',
+    height: 96,
+    paddingVertical: 24,
+    paddingHorizontal: 19
+  },
+  memoTitle: {
+    fontSize: 20,
+    lineHeight: 32,
+    color: '#ffffff'
+  },
+  memoDate: {
+    fontSize: 12,
+    lineHeight: 16,
+    color: '#ffffff'
+  },
+  memoBody: {
+    paddingVertical: 32,
+    paddingHorizontal: 27
+  },
+  memoBodyText: {
+    fontSize: 16,
+    lineHeight: 24,
+    color: '#000000'
+  }
+})
+
+export default Detail
