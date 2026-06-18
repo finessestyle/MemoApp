21bf64dfa87d4f6b7ef9c160959fb5b15e338d9a
finessestyle <finessestyle1@gmail.com>
2026-06-18 19:11:55 +0900

    useLocalSerchParams

--- src/app/memo/edit.tsx
+++ src/app/memo/edit.tsx
@@ -1,23 +1,56 @@
-import { View, StyleSheet, TextInput, KeyboardAvoidingView } from 'react-native'
+import { View, StyleSheet, TextInput, KeyboardAvoidingView, Alert } from 'react-native'
 import { router } from 'expo-router'
+import { useLocalSearchParams } from 'expo-router'
+import { doc, getDoc, setDoc, Timestamp } from 'firebase/firestore'
+import { useState, useEffect } from 'react'
+import { auth, db } from '../../config'
 import CircleButton from '../../components/CircleButton'
 import Icon from '../../components/Icon'
 
-const handlePress = () => {
-  router.back()
+const handlePress = (id: string, bodyText: string) => {
+  if (auth.currentUser === null) return
+  const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+  setDoc(ref, {
+    bodyText,
+    updatedAt: Timestamp.fromDate(new Date())
+  })
+  .then (() => {
+    router.back()
+  })
+  .catch ((error) => {
+    Alert.alert(error)
+  })
 }
 
 const Edit = () => {
+  const id = String(useLocalSearchParams().id)
+  const [bodyText, setBodyText] = useState('')
+  useEffect(() => {
+    if (auth.currentUser === null) return
+    const ref = doc(db, `users/${auth.currentUser.uid}/memos`, id)
+    getDoc(ref)
+      .then( (docRef) => {
+        const remoteBodyText = docRef?.data()?.bodyText
+        setBodyText(remoteBodyText)
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }, [])
+
+
   return (
     <KeyboardAvoidingView behavior='height' style={styles.container}>
       <View style={styles.inputContainer}>
         <TextInput
-          value={'買い物リスト\n・りんご'}
+          value={bodyText}
           style={styles.input}
           multiline
+          autoFocus
+          onChangeText={(text) => {setBodyText(text)}}
         />
       </View>
-      <CircleButton onPress={handlePress} style={{ bottom: 40 }}>
+      <CircleButton onPress={() => handlePress(id, bodyText)} style={{ bottom: 40 }}>
         <Icon name='check' size={40} color='#ffffff' />
       </CircleButton>
     </KeyboardAvoidingView>
